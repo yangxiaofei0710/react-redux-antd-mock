0337d5e052a6e0f188f8600b902dccaaaab72601
wb-yxf273267 <wb-yxf273267@antfin.com>
2017-08-08 16:45:05 +0800

    修改mock数据模板

--- src/mock/mock.js
+++ src/mock/mock.js
@@ -3,7 +3,7 @@ import Mock from "mockjs";
 Mock.mock("setUp.json",{ //拦截的ajax请求接口
 //    数据模板
     "list|10": [{
-        'name': /[a-z]{4}\.(尼古拉斯)/,
+        'name': "@NAME",
         'age|+1': 20,
         'email': '@EMAIL'
     }]
